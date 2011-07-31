b1ce9b43d9389ffdcb5aee9c0e42b4a3147148d6
David Grant <seizethedave@gmail.com>
2011-07-30 19:12:46 -0500

    Better skipping of seen/uninteresting crap.

--- yammer.py
+++ yammer.py
@@ -6,18 +6,29 @@ import mechanize
 import summarize
 from BeautifulSoup import *
 
+kMaxPara = 3
+kLinkQueueSize = 12
+kSeenUrlQueueSize = 64
+kSeenSummaryQueueSize = 22
+
+kMechanizeTimeout = 10.0
+
+# These are known to provide uninteresting content.
+
+kShitDomains = frozenset(('google.com', 'adobe.com', 'amazon.com',
+ 'microsoft.com', 'youtube.com'))
 
 def PatchSoupTag():
    """
    Monkeypatch BeautifulSoup Tag to handle spaces better.
-   https://code.launchpad.net/~mjumbewu/beautifulsoup/text-white-space-fix/+merge/62629
+   http://bit.ly/ndELQk
    """
-   def getText(soup, separator=u""):
-      if not len(soup.contents):
+   def getText(tag, separator=u""):
+      if not len(tag.contents):
          return u""
-      stopNode = soup._lastRecursiveChild().next
+      stopNode = tag._lastRecursiveChild().next
       strings = []
-      current = soup.contents[0]
+      current = tag.contents[0]
       while current is not stopNode:
          if isinstance(current, NavigableString):
             strings.append(current)
@@ -32,9 +43,7 @@ def PatchSoupTag():
 
 PatchSoupTag()
 
-kMaxPara = 3
 
-kStartConcepts = ('creepy',)
 
 def FlattenSoupTags(paras):
    for p in paras:
@@ -44,8 +53,19 @@ def FlattenSoupTags(paras):
          else:
             yield atom.text
 
+
+def CleanSoup(soup):
+   stuffToDelete = ("script", "style", "header", "footer", "nav",
+    "h1", "h2", "h3", "h4", "h5", "h6")
+
+   for tag in stuffToDelete:
+      for item in soup.findAll(tag):
+         item.extract()
+
 def GetSummary(soup):
-   paragraphs = soup.findAll('p')[:kMaxPara]
+   paragraphs = soup.findAll('p')
+   random.shuffle(paragraphs)
+   paragraphs = paragraphs[:kMaxPara]
    t = FlattenSoupTags(paragraphs)
    text = u''.join(t)
    summarizer = summarize.SimpleSummarizer()
@@ -58,44 +78,71 @@ if '__main__' == __name__:
    mech.addheaders = [('User-Agent',
     'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1')]
 
-   linkQueue = deque([], 10)
+   linkQueue = deque([
+    "http://www.ishouldbeworking.com/creepy.htm",
+    "http://secretcrypt.com/newcrypt/bizarre/linkspage.html",
+    "http://www.parapsychologydegrees.com/",
+    "http://www.scaryscreaming.com/ranesreads.html",
 
-   startUrl = "http://en.wikipedia.org/wiki/MRSA"
+    ], kLinkQueueSize)
 
-   request = startUrl
+   seenUrls = deque([], kSeenUrlQueueSize)
+   seenSummaries = deque([], kSeenSummaryQueueSize)
 
-   while True:
+   def DecentLookingLink(link):
+      absUrl = link.absolute_url
+
+      return (
+       not link.url.startswith('#')
+       and absUrl.startswith('http')
+       and absUrl not in seenUrls
+       and all(shitDomain not in absUrl for shitDomain in kShitDomains)
+      )
+
+   while len(linkQueue) > 0:
+      nextUrl = linkQueue.pop()
+
+      if nextUrl in seenUrls:
+         continue
+      seenUrls.appendleft(nextUrl)
+
+      print "Processing URL: %s" % nextUrl
+
       try:
-         mech.open(request)
+         mech.open(nextUrl, timeout = kMechanizeTimeout)
          data = mech.response().read()
       except:
-         data = None
-
-      if not data:
-         request = mech.click_link(linkQueue.pop())
          continue
 
-      print mech.geturl()
-
-      if not mech.viewing_html():
-         data = None
+      if not data or not mech.viewing_html():
          continue
 
       try:
          soup = BeautifulSoup(data)
       except UnicodeEncodeError:
-         data = None
          continue
 
-      print GetSummary(soup)
-      links = filter(lambda l: not l.url.startswith('#'), mech.links())
+      CleanSoup(soup)
+
+      summary = GetSummary(soup)
+
+      if not summary or summary in seenSummaries:
+         continue
+
+      seenSummaries.appendleft(summary)
+
+      print summary
+
+      try:
+         links = filter(DecentLookingLink, mech.links())
+      except:
+         continue
 
       random.shuffle(links)
 
-      for link in links[:4]:
-         linkQueue.appendleft(link)
+      for link in links[:2]:
+         linkQueue.appendleft(link.absolute_url)
 
-      print len(linkQueue)
+      print "Link queue size: %d" % len(linkQueue)
 
-      request = mech.click_link(linkQueue.pop())
-
+   print "Link queue exhausted."
